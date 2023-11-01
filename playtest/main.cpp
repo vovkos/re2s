c4944c161e3c55807a1160af1e025c5a00f0b320
Vladimir Gladkov (office-win10) <vovkos@gmail.com>
2023-11-01 14:20:59 +0800

    Cleanup playtest; everything is fully functional

--- playtest/main.cpp
+++ playtest/main.cpp
@@ -6,12 +6,17 @@
 #include "re2.h"
 #include "re2/sm.h"
 #include "re2/set.h"
+#include "absl/strings/str_cat.h"
+
+void print_submatches(const absl::string_view* submatches, size_t count) {
+	for (size_t i = 0; i < count; i++)
+		printf("  submatch[%zd]: '%s'\n", i, std::string(submatches[i]).c_str());
+}
 
 void print_re2_sm_submatches(const re2::RE2::SM& sm, int match_id, absl::string_view match) {
 	enum {
 		MaxSubmatchCount = 8
 	};
-
 
 	const re2::RE2::SM::Module* module = sm.kind() == re2::RE2::SM::kSingleRegexp ? sm.module() : sm.switch_case(match_id);
 	size_t capture_count = module->capture_count() + 1;
@@ -22,42 +27,45 @@ void print_re2_sm_submatches(const re2::RE2::SM& sm, int match_id, absl::string_
 	if (!result)
 		printf("can't capture submatches\n");
 	else
-		for (size_t i = 0; i < capture_count; i++)
-			printf("  submatch[%zd]: '%s'\n", i, std::string(submatches[i]).c_str());
+		print_submatches(submatches, capture_count);
 }
 
 int main() {
-	printf("main\n");
+	printf("re2::RE2::SM playtest\n");
 
 	re2::RE2::Options options;
 	options.set_longest_match(true);
 	options.set_case_sensitive(false);
+	options.set_multi_line(true);
 
-	const char single_pattern[] = "(?m)(^abc$)";
+	const char single_pattern[] = "^abc(\\d+)$";
 	const char* switch_patterns[] = {
-		"(?m)(^abc$)",
-		"(?m)(^def$)",
-		"(?m)(^ghi$)",
+		"^abc(\\d+)$",
+		"^def(\\d+)$",
+		"^ghi(\\d+)$",
 	};
 
-	const char text[] = " \nghi\n \ndef\n \nabc\n ";
+	const char text[] = "ghi12\n \ndef34\n \nabc56";
 	size_t length = sizeof(text) - 1;
 	std::string match;
 
 	do {
-		printf("using re2::RE2...\n");
+		printf("\nusing re2::RE2...\n");
 
-		re2::RE2 re(single_pattern, options);
+		re2::RE2 re(absl::StrCat("(", single_pattern, ")"));
 		absl::string_view match;
-		bool result = re.RE2::PartialMatch(text, re, &match);
+		absl::string_view submatch;
+		bool result = re.RE2::PartialMatch(text, re, &match, &submatch);
 		if (!result)
 			printf("not found\n");
-		else
+		else {
 			printf("match at %zd:%zd '%s'\n", match.begin() - text, match.end() - text, std::string(match).c_str());
+			print_submatches(&submatch, 1);
+		}
 	} while (0);
 
 	do {
-		printf("using re2::RE2::SM (single regexp, full text)...\n");
+		printf("\nusing re2::RE2::SM (single regexp, full text)...\n");
 
 		re2::RE2::SM sm;
 		bool result = sm.create(single_pattern, options);
@@ -88,7 +96,7 @@ int main() {
 	} while (0);
 
 	do {
-		printf("using re2::RE2::SM (regexp switch, full text)...\n");
+		printf("\nusing re2::RE2::SM (regexp switch, full text)...\n");
 
 		re2::RE2::SM sm;
 		sm.create_switch(options);
@@ -115,8 +123,8 @@ int main() {
 				state.match_start_offset(),
 				state.match_end_offset(),
 				match.c_str(),
-				state.match_last_char_,
-				state.match_next_char_
+				state.match_last_char(),
+				state.match_next_char()
 			);
 
 			print_re2_sm_submatches(sm, state.match_id(), match);
@@ -124,7 +132,7 @@ int main() {
 	} while (0);
 
 	do {
-		printf("using re2::RE2::SM (regexp switch, char-by-char)...\n");
+		printf("\nusing re2::RE2::SM (regexp switch, char-by-char)...\n");
 
 		re2::RE2::SM sm;
 		sm.create_switch(options);
@@ -144,33 +152,18 @@ int main() {
 		while (p < eof) {
 			size_t chunk_length = 1;
 			re2::RE2::SM::ExecResult result = sm.exec(&state, absl::string_view(p, chunk_length));
+			assert(result != re2::RE2::SM::kMatch && "forward match can't happen with the current pattern + chunk_length");
 			switch (result) {
 			case re2::RE2::SM::kContinue:
 				p += chunk_length;
 				break;
 
 			case re2::RE2::SM::kMismatch:
-				printf("mismatch\n");
+				printf("not found\n");
 				return -1;
 
-			case re2::RE2::SM::kMatch:
-				match.assign(text + state.match_start_offset(), state.match_length());
-				printf(
-					"fmatch id: %d at %zd:%zd '%s' { 0x%02x; 0x%02x }\n",
-					state.match_id(),
-					state.match_start_offset(),
-					state.match_end_offset(),
-					match.c_str(),
-					state.match_last_char_,
-					state.match_next_char_
-				);
-
-				print_re2_sm_submatches(sm, state.match_id(), match);
-				p = text + state.match_end_offset();
-				break; // keep searching forward
-
 			case re2::RE2::SM::kContinueBackward:
-				printf("end-of-match @%zd\n", state.match_end_offset());
+				printf("end-of-match id: %d at %zd\n", state.match_id(), state.match_end_offset());
 
 				while (p > text) {
 					re2::RE2::SM::ExecResult result = sm.exec(&state, absl::string_view(p - chunk_length, chunk_length));
@@ -180,18 +173,6 @@ int main() {
 						break;
 
 					case re2::RE2::SM::kMatch:
-						match.assign(text + state.match_start_offset(), state.match_length());
-						printf(
-							"rmatch id: %d at %zd:%zd '%s' { 0x%02x; 0x%02x }\n",
-							state.match_id(),
-							state.match_start_offset(),
-							state.match_end_offset(),
-							match.c_str(),
-							state.match_last_char_,
-							state.match_next_char_
-						);
-
-						print_re2_sm_submatches(sm, state.match_id(), match);
 						p = text; // break out of the parent while loop
 						break;
 
@@ -200,7 +181,22 @@ int main() {
 						return -2;
 					}
 				}
+				ABSL_FALLTHROUGH_INTENDED;
 
+			case re2::RE2::SM::kMatch:
+				match.assign(text + state.match_start_offset(), state.match_length());
+				printf(
+					"+match id: %d at %zd:%zd '%s' { 0x%02x; 0x%02x }\n",
+					state.match_id(),
+					state.match_start_offset(),
+					state.match_end_offset(),
+					match.c_str(),
+					state.match_last_char(),
+					state.match_next_char()
+				);
+
+				print_re2_sm_submatches(sm, state.match_id(), match);
+				printf("keep searching...\n");
 				p = text + state.match_end_offset();
 				break;
 
